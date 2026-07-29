4ad5f0d6656bba8f5f314b7ce24f3fa8572bb462
Adriel Jacobus Pereira <adrieljp2005@hotmail.com>
2026-07-29 03:48:05 -0300

    // WIP

--- Backup/Services/BrowserService.cs
+++ Backup/Services/BrowserService.cs
@@ -1,26 +1,27 @@
 ﻿using System.Diagnostics;
 using Backup.Models;
+using Microsoft.Playwright; // dotnet add package Microsoft.Playwright
 
 namespace Backup.Services;
 
 public static class BrowserService
 {
-    public static void OpenLinks()
+    public static async Task OpenLinks()
     {
         try
         {
             foreach (var link in Config.Configs.Links)
             {
-                Process.Start(new ProcessStartInfo
+                if (link.Contains("us.ugreen.com"))
+                {
+                    await Ugreen();
+                }
+
+                /*Process.Start(new ProcessStartInfo
                 {
                     FileName = $"{link}",
                     UseShellExecute = true
-                });
-
-                if (link.Contains("us.ugreen.com"))
-                {
-                    Console.WriteLine("DIGITE \"80889\" NA BARRA DE PESQUISA DO SITE \"us.ugreen.com\" PARA BAIXAR O DRIVER DO MODELO CERTO!");
-                }
+                });*/
             }
         }
 
@@ -28,5 +29,29 @@ public static class BrowserService
         {
             Console.WriteLine($"Erro: {ex.Message}");
         }
+    }
+
+
+    private static async Task Ugreen()
+    {
+        using var playwright = await Playwright.CreateAsync();
+
+        await using var browser = await playwright.Chromium.LaunchAsync(new()
+        {
+            Channel = "msedge",
+            Headless = false // true = não mostra o navegador
+        });
+
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://us.ugreen.com/pages/download");
+
+        var input = page.GetByRole(AriaRole.Textbox, new() { Name = "Search by Product SKU/Model" });
+
+        await input.ClickAsync();
+
+        await input.PressSequentiallyAsync("80889");
+
+        await input.PressAsync("Enter");
     }
 }
